d509f34fe6d2a6b7db6387064fd1a25afada3272
Yoni Smolin <Yoni.Smolin@gmail.com>
2015-08-01 14:40:21 +0300

    Add a client for kinect depth. Update Makefile accordingly.

---
+++ Technion/DepthClient.cpp
@@ -0,0 +1,44 @@
+/*
+ * ClientMain.cpp -- client program
+ */
+
+#include "Client.hpp"
+
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+#define PORT "3490"
+#define SERVER_NAME "localhost"
+
+#define ROWS 424
+#define COLS 512
+
+#define NUM_FRAMES 1000
+
+int main(int argc, char** argv)
+{
+	uchar imageArray[ROWS*COLS];
+
+	Client client;
+	cout << "Initialized client successfully" << endl;
+	client.ConnectToServer(SERVER_NAME, PORT);
+
+	namedWindow("ramp",1);
+
+	// sending loop
+	for(int frame = 0; frame < NUM_FRAMES; frame ++)
+	{
+		client.ReceiveMatrix((char*)imageArray, ROWS, COLS); 
+		Mat image = Mat(ROWS, COLS, CV_8UC1, imageArray);
+		imshow("ramp", image);
+		waitKey(1);
+	}
+
+	client.CloseConnection();
+	cout << "Connection closed" << endl;
+	
+	return 0;
+}
