87be6d459a77348aedf5b7bbf1ba14c929e07d85
unknown <t-yonis@T-YONIS-LAP3.middleeast.corp.microsoft.com>
2015-08-08 15:01:05 +0300

    First commit of a basic windows client

---
+++ windows/Main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <conio.h>
+
+#include "Client.h"
+
+#define SERVER_NAME "192.168.137.2"
+#define PORT "3490"
+
+#define MESSAGE_LENGTH 128
+
+using namespace std;
+
+int main()
+{
+	char message[MESSAGE_LENGTH];
+
+	Client client;
+	cout << "Initialized client successfully" << endl;
+
+	if (client.ConnectToServer(SERVER_NAME, PORT) == 1)
+		return 1;
+	cout << "Successfully connected to server" << endl;
+
+	int bytesReceived = client.ReceiveMessage(message, MESSAGE_LENGTH);
+
+	if (bytesReceived < 0)
+		return 1;
+
+	if (bytesReceived > 0)
+	{
+		message[bytesReceived] = '\0';
+		cout << "Received the following message: " << message << endl;
+	}
+	
+	client.CloseConnection();
+	cout << "Successfully closed client connection" << endl;
+
+	cout << "Type any key to exit" << endl;
+
+	while (!_kbhit());
+
+	return 0;
+}
