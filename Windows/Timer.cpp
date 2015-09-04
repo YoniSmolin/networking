87fb09a3ea4d0b0c7c91bff2cfb1678c94e171e2
unknown <yonis@VSM-PC-132.eed.ef.technion.ac.il>
2015-09-04 16:19:37 +0300

    Update timer to produce more accurate telemtry

--- Windows/Timer.cpp
+++ Windows/Timer.cpp
@@ -11,23 +11,24 @@ Timer::Timer(char* name, int windowSize) : _sampleCounter(0), _accumulatedTime(0
 
 void Timer::Start()
 {
-	QueryPerformanceCounter(&_start);
+	if (_sampleCounter == 0)
+		QueryPerformanceCounter(&_start);
+
 	_sampleCounter++;
 }
 
 void Timer::Stop(int numBytesMoved)
 {
-	QueryPerformanceCounter(&_end);
-	_accumulatedTime += _end.QuadPart - _start.QuadPart;
 	_accumulatedBytes += numBytesMoved;
 	 
 	if (_sampleCounter == _windowSize)
 	{
-		float cycle = (_accumulatedTime / _frequency.QuadPart) / _windowSize;
+		QueryPerformanceCounter(&_end);
+		float accumulatedTime = _end.QuadPart - _start.QuadPart;
+		float cycle = (accumulatedTime / _frequency.QuadPart) / _windowSize;
 		printf("%s : Rate - %2.1f [Hz], Cycle - %2.1f [mSec]\n", _name.c_str(), 1/cycle, 1000 * cycle);
-		printf("%s : Bandwidth - %2.1f [Mbps]\n", _name.c_str(), (_accumulatedBytes*8 / (_accumulatedTime / _frequency.QuadPart))  / (1 << 20));
- 		_accumulatedTime = 0;
-		_accumulatedBytes = 0;
+		printf("%s : Bandwidth - %2.1f [Mbps]\n", _name.c_str(), (_accumulatedBytes*8 / (accumulatedTime / _frequency.QuadPart))  / (1 << 20));
+ 		_accumulatedBytes = 0;
 		_sampleCounter = 0;
 	}
 }
